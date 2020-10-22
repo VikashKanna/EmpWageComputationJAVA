6aaffcf7434a06ea6896cefdb87f6c184ef998e0
Vikash Kanna <Enter Git Registered Email id>
2020-10-23 00:29:45 +0530

    Constant MAX_HOURS_IN_MONTH

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,12 +3,16 @@ public static final int IS_PART_TIME = 1;
         public static final int IS_FULL_TIME = 2;
         public static final int EMP_RATE_PER_HOUR = 20;
     public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
 	public static void main(String[] args) {
    	 System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION");
-		int empHrs = 0;
-		int empWage = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
 		int totalEmpWage = 0;
-		for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
 		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 		switch (empCheck) {
 			case IS_PART_TIME:
@@ -19,11 +23,12 @@ public static final int IS_PART_TIME = 1;
 				break;
 			default:
 				empHrs = 0;
+			totalEmpHrs += empHrs;
+			System.out.println("Day#: " +totalWorkingDays + "Emp Hr:" +empHrs);
 			}
-			empWage = empHrs * EMP_RATE_PER_HOUR;
-			totalEmpWage += empWage;
-			System.out.println("Emp Wage: " +empWage);
+			int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 		}
+
 			System.out.println("Total Emp Wage: " +totalEmpWage);
 	}
 
