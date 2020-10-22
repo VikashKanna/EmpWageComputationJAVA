cfd3c049ddb98f6eed104716d57d4f71a9e0a31e
Vikash Kanna <Enter Git Registered Email id>
2020-10-22 23:59:23 +0530

    Attendance Taken

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,5 +2,12 @@ public class EmpWageComputation {
 
     public static void main(String[] args) {
         System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION");
+int IS_FULL_TIME = 1;
+	double empCheck = Math.floor(Math.random() * 10) % 2;
+	if (empCheck == IS_FULL_TIME)
+		System.out.println("Employee is Present");
+	else
+		System.out.println("Employee is Absent");
+
     }
 }
